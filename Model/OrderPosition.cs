d34833ef2fe84506b7400bb6768723e9299318ad
pk <polsl>
2024-03-06 18:54:16 +0100

    Model Danych-Migracja

---
+++ Model/OrderPosition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class OrderPosition
+    {
+        public int ID { get; set; }
+        public int OrderID { get; set; }
+        [ForeignKey(nameof(OrderID))]
+        public Order Order { get; set; }
+        public int Amount { get; set; }
+        public double Price { get; set; }
+    }
+}
